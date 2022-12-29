08bb0e570f61541da1f54b58146b45856c96dfe2
Yordanov <tyordanov@amc.intra>
2022-12-29 15:18:45 +0100

    minor bugfix

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,4 +1,4 @@
-ICD_CODE_DEFS_PATH="../MIMIC/d_DIAG_PROCED.csv"
+ICD_CODE_DEFS_PATH="../../data/d_DIAG_PROCED.csv"
 DATA_PATH = "../../data/diagnoses_procedures_icd_icu_staydetail.json" #test_2k.json
 W2V_PATH = "../../data/GoogleNews-vectors-negative300.bin.gz"
 
