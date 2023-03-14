636c33116a8cc361acd5e42f5b15325d9f69b14b
Yordanov <tyordanov@amc.intra>
2023-03-14 18:14:42 +0100

    updates README

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,6 +1,8 @@
 ICD_CODE_DEFS_PATH="../../data/d_DIAG_PROCED.csv"
+IN_DATA_PATH = "../../data/diagnoses_procedures_icd_icu_staydetail.csv"
 DATA_PATH = "../../data/diagnoses_procedures_icd_icu_staydetail.json" #test_2k.json
 W2V_PATH = "../../data/GoogleNews-vectors-negative300.bin.gz"
+IN_DATA_PATH_VITALS = "../../data/vitals.csv"
 
 W2V_IS_BINARY = True
 LOAD_EMBEDDINGS = True
